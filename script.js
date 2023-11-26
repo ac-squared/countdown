146096b4158ad08a8babda357bc0397f933dc1b6
ericrao17 <128321795+ericrao17@users.noreply.github.com>
2023-11-26 16:39:39 -0500

    added an expired at the end and fixed the minute bug thing

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var date = new Date("Dec 22, 2023 15:30:00").getTime();
+var now = new Date().getTime();
 
 var loop = setInterval(function() {
-  var currentDate = new Date().getTime();
-  var distance = date - currentDate;
+  var now = new Date().getTime();
+  var distance = date - now;
 
-  document.getElementById("demo").innerHTML = Math.floor(distance / 86400000) + "d " + Math.floor((distance % 86400000) / 60000) + "m " + Math.floor((distance % 60000) / 1000) + "s ";
+  document.getElementById("demo").innerHTML = Math.floor(distance / 86400000) + "d " + Math.floor((distance % 86400000) / 3600000) + "h " + Math.floor((distance % 600000) / 60000) + "m " + Math.floor((distance % 60000) / 1000) + "s ";
 
-  // check if countdown is over
-  if (distance < 0) {
-    clearInterval(loop);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
+  if (distance <= 0) {
+      clearInterval(interval);
+      document.getElementById("demo").innerHTML = "EXPIRED";
 }, 1000);
