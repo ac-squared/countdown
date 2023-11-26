eb3fe1645b3fd7e76a7d1379fa33634102667e37
ericrao17 <128321795+ericrao17@users.noreply.github.com>
2023-11-26 16:43:36 -0500

    forgot a bracket

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var date = new Date("Dec 22, 2023 15:30:00").getTime();
-var now = new Date().getTime();
 
-var loop = setInterval(function() {
+var interval = setInterval(function() {
   var now = new Date().getTime();
   var distance = date - now;
 
-  document.getElementById("demo").innerHTML = Math.floor(distance / 86400000) + "d " + Math.floor((distance % 86400000) / 3600000) + "h " + Math.floor((distance % 600000) / 60000) + "m " + Math.floor((distance % 60000) / 1000) + "s ";
+  document.getElementById("demo").innerHTML = Math.floor(distance / 86400000) + "d " + Math.floor((distance % 86400000) / 3600000) + "h " + Math.floor((distance % 3600000) / 60000) + "m " + Math.floor((distance % 60000) / 1000) + "s ";
 
   if (distance <= 0) {
-      clearInterval(interval);
-      document.getElementById("demo").innerHTML = "EXPIRED";
+    clearInterval(interval);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
 }, 1000);
